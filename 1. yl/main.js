abb8fa591d380259928dbc6a525e7ff179e1dc61
Peter Saan <peter.saan@tptlive.ee>
2024-01-29 08:35:10 +0200

    Renamed for better syntax

--- 1. yl/main.js
+++
@@ -1,9 +0,0 @@
-fetch('https://api.met.no/weatherapi/locationforecast/2.0/compact?lat=59.44&lon=24.75')
-  .then(res => {
-    return res.json();
-  })
-  .then(res => {
-      for(let i = 0; i < res.properties.timeseries.length; i++) {
-          console.log(res.properties.timeseries[i].time, res.properties.timeseries[i].data.instant.details.air_temperature + "C");
-      }
-  });
