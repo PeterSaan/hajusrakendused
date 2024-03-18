d54a5f168ac02809afbe3b19306ab7607ffd5080
PeterSaan <saanpeter@gmail.com>
2024-03-18 16:33:45 +0200

    On the busssss

--- spare-parts/main.js
+++ spare-parts/main.js
@@ -6,12 +6,12 @@ const app = express();
 const port = 3000;
 const readStream = createReadStream('../andmed/LE.txt');
 let listOfParts = [];
-const startPage = 1;
+let currentPage = 1;
 const itemsPerPage = 30;
 let idSearch;
 let nameSearch;
 
-app.use(express.json());
+app.use(express.json({ strict: false }));
 
 readStream.pipe(csv({ separator: '\t'}))
     .on('data', (data) => listOfParts.push(data))
@@ -21,21 +21,25 @@ readStream.on('error', (err) => {
     console.log(`Error: ${err}`);
 });
 
-app.get('/', (req, res) => {
+app.get('/spare-parts/:name', (req, res) => {
     nameSearch = req.params.name;
     idSearch = req.params.id;
 
-    if (Buffer.from(nameSearch)) {
-        res.write(listOfParts.filter(part => part.name.includes(nameSearch)));
-    } else if (Buffer.from(idSearch)) {
-        res.write(listOfParts.filter(part => part.serialNumber.includes(idSearch)));
-    } else if (Buffer.from(nameSearch) && Buffer.from(idSearch)) {
-        res.write(listOfParts.filter(part => part.name.includes(nameSearch) && part.serialNumber.includes(idSearch)));
-    } else {
-        // See pole päris õige, aga siit saad idee
-        res.write(listOfParts.slice(startPage, itemsPerPage));
-    }
+    // if (nameSearch) {
+    //     res.write(listOfParts.filter(part => part.name.includes(nameSearch)));
+    // } else if (idSearch) {
+    //     res.write(listOfParts.filter(part => part.serialNumber.includes(idSearch)));
+    // } else if (nameSearch && idSearch) {
+    //     res.write(listOfParts.filter(part => part.name.includes(nameSearch) && part.serialNumber.includes(idSearch)));
+    // } else {
+    //     See pole päris õige, aga siit saad idee
+    //     res.write(listOfParts.slice(startPage, itemsPerPage));
+    // }
     
+    res.write(listOfParts.filter(part => part.name.includes(nameSearch)))
+
+    console.log(nameSearch);
+    console.log(idSearch);
 
     res.end();
 });
@@ -49,5 +53,5 @@ app.get('/spare-parts', (req, res) => {
 });
 
 app.listen(port, () => {
-    console.log(`App listening on port http://localhost:${port}`);
+    console.log(`App listening on http://localhost:${port}`);
 });
